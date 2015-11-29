8f0355db492bf1767dcafb49e904445404fd0f5d
Vytenis Bivainis <monrealis@gmail.com>
2015-11-29 21:19:26 +0200

    Extract smaller methods.

--- src/main/java/eu/vytenis/cv/fo/FoTableBuilder.java
+++ src/main/java/eu/vytenis/cv/fo/FoTableBuilder.java
@@ -1,5 +1,9 @@
 package eu.vytenis.cv.fo;
 
+import static java.util.Arrays.asList;
+
+import java.util.List;
+
 import org.w3._1999.xsl.format.Table;
 import org.w3._1999.xsl.format.TableBody;
 import org.w3._1999.xsl.format.TableCell;
@@ -9,17 +13,37 @@ import org.w3._1999.xsl.format.TableRow;
 import eu.vytenis.cv.builders.Builder;
 
 public class FoTableBuilder implements Builder<Table> {
+	private Table table = new Table();
+
 	public Table build() {
-		Table table = new Table();
-		table.getTableColumn().add(new TableColumn());
-		table.getTableColumn().add(new TableColumn());
-		TableBody body = new TableBody();
-		table.getTableBody().add(body);
-		TableRow row = new TableRow();
-		row.getTableCell().add(new TableCell());
-		row.getTableCell().add(new TableCell());
-		body.getTableRow().add(row);
+		addColumns();
+		addBody();
 		return table;
 	}
 
+	private void addBody() {
+		table.getTableBody().add(createBody());
+	}
+
+	private void addColumns() {
+		table.getTableColumn().add(new TableColumn());
+		table.getTableColumn().add(new TableColumn());
+	}
+
+	private TableBody createBody() {
+		TableBody body = new TableBody();
+		body.getTableRow().addAll(createRows());
+		return body;
+	}
+
+	private List<TableRow> createRows() {
+		return asList(createRow());
+	}
+
+	private TableRow createRow() {
+		TableRow row = new TableRow();
+		row.getTableCell().add(new TableCell());
+		row.getTableCell().add(new TableCell());
+		return row;
+	}
 }
