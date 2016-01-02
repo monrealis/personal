0249a312e17f79e51a5d27b682e19bf3465d1092
Vytenis Bivainis <monrealis@gmail.com>
2016-01-03 00:45:03 +0200

    Add tests for sections.

--- src/test/java/eu/vytenis/cv/localizer/MapperBasedSelectorTest.java
+++ src/test/java/eu/vytenis/cv/localizer/MapperBasedSelectorTest.java
@@ -1,16 +1,24 @@
 package eu.vytenis.cv.localizer;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import eu.vytenis.cv.TAdditionalInformationSection;
 import eu.vytenis.cv.TLString;
-import eu.vytenis.cv.localizer.MapperBasedSelector;
 
 public class MapperBasedSelectorTest {
 	private TLString string = new TLString();
+	private TAdditionalInformationSection section1 = new TAdditionalInformationSection();
+	private TAdditionalInformationSection section2 = new TAdditionalInformationSection();
+	private List<TAdditionalInformationSection> sections = new ArrayList<>(
+			asList(section1, section2));
 	private String language = null;
 
 	@Before
@@ -37,5 +45,29 @@ public class MapperBasedSelectorTest {
 
 	private String getString() {
 		return new MapperBasedSelector(language).getString(asList(string));
+	}
+
+	@Test
+	public void getSections_returnsForNullLanguage() {
+		assertEquals(asList(section1, section2), getSections());
+	}
+	
+	@Test
+	public void getSections_doesNotReturnIfNotFound() {
+		language = "lt";
+		assertEquals(emptyList(), getSections());
+	}
+	
+	@Test
+	public void getSections_returnsByLanguage() {
+		language = "lt";
+		section2.setLanguage(language);
+		assertEquals(asList(section2), getSections());
+	}
+
+
+	private List<TAdditionalInformationSection> getSections() {
+		return new MapperBasedSelector(language)
+				.getAdditionalInformationSection(sections);
 	}
 }
