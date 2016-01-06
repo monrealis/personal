012eb05906fccbca2fa7fbdd0747cf3a351411a9
Vytenis Bivainis <monrealis@gmail.com>
2016-01-07 00:14:35 +0200

    Refactor.

--- src/main/java/eu/vytenis/cv/messages/PropertyMessageSource.java
+++ src/main/java/eu/vytenis/cv/messages/PropertyMessageSource.java
@@ -2,22 +2,36 @@ package eu.vytenis.cv.messages;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class PropertyMessageSource {
 	private Map<String, String> messages = new TreeMap<>();
 
 	public PropertyMessageSource(String resource) {
-		Properties p = new Properties();
+		loadEntries(resource).forEach(this::put);
+	}
+
+	private void put(Entry<Object, Object> entry) {
+		String key = entry.getKey().toString();
+		String value = entry.getValue().toString();
+		messages.put(key, value);
+	}
+
+	private Set<Entry<Object, Object>> loadEntries(String resource) {
+		return load(resource).entrySet();
+	}
+
+	private Properties load(String resource) {
 		try {
+			Properties p = new Properties();
 			p.load(getClass().getResourceAsStream(resource));
+			return p;
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-		p.entrySet().forEach(
-				e -> messages.put(e.getKey().toString(), e.getValue()
-						.toString()));
 	}
 
 	public String getMessage(MessageCode messageCode) {
