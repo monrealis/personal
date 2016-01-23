dcf108670d7cce8aa8dc3656adf351fca2e27a87
Vytenis Bivainis <monrealis@gmail.com>
2016-01-24 00:20:32 +0200

    Move functionality to BaseUnmarshaller.

--- src/main/java/eu/vytenis/cv/xmlio/BaseUnmarshaller.java
+++ src/main/java/eu/vytenis/cv/xmlio/BaseUnmarshaller.java
@@ -1,5 +1,13 @@
 package eu.vytenis.cv.xmlio;
 
+import java.io.InputStream;
+import java.io.StringReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 public class BaseUnmarshaller<T> {
 	private final Class<T> type;
 	private final Class<?>[] objectFactoryTypes;
@@ -7,6 +15,40 @@ public class BaseUnmarshaller<T> {
 	public BaseUnmarshaller(Class<T> type, Class<?>... objectFactoryTypes) {
 		this.type = type;
 		this.objectFactoryTypes = objectFactoryTypes;
+	}
+
+	protected T unmarshall(InputStream xml) {
+		try {
+			return tryUnmarshall(xml);
+		} catch (JAXBException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected T unmarshall(String xml) {
+		try {
+			return tryUnmarshall(xml);
+		} catch (JAXBException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private T tryUnmarshall(InputStream xml) throws JAXBException {
+		return extract((JAXBElement<?>) createUnmarshaller().unmarshal(xml));
+	}
+
+	private T tryUnmarshall(String xml) throws JAXBException {
+		StringReader r = new StringReader(xml);
+		return extract((JAXBElement<?>) createUnmarshaller().unmarshal(r));
+	}
+
+	private T extract(JAXBElement<?> element) {
+		return type.cast(element.getValue());
+	}
+
+	private Unmarshaller createUnmarshaller() throws JAXBException {
+		JAXBContext c = new ContextCache().getContext(objectFactoryTypes);
+		return c.createUnmarshaller();
 	}
 
 }
--- src/main/java/eu/vytenis/cv/xmlio/CvUnmarshaller.java
+++ src/main/java/eu/vytenis/cv/xmlio/CvUnmarshaller.java
@@ -1,12 +1,6 @@
 package eu.vytenis.cv.xmlio;
 
 import java.io.InputStream;
-import java.io.StringReader;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
 
 import eu.vytenis.cv.CV;
 import eu.vytenis.cv.ObjectFactory;
@@ -16,38 +10,8 @@ public class CvUnmarshaller extends BaseUnmarshaller<CV> {
 		super(CV.class, ObjectFactory.class);
 	}
 
+	@Override
 	public CV unmarshall(InputStream xml) {
-		try {
-			return tryUnmarshall(xml);
-		} catch (JAXBException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	public CV unmarshall(String xml) {
-		try {
-			return tryUnmarshall(xml);
-		} catch (JAXBException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private CV tryUnmarshall(InputStream xml) throws JAXBException {
-		return asCv((JAXBElement<?>) createUnmarshaller().unmarshal(xml));
-	}
-
-	private CV tryUnmarshall(String xml) throws JAXBException {
-		StringReader r = new StringReader(xml);
-		return asCv((JAXBElement<?>) createUnmarshaller().unmarshal(r));
-	}
-
-	private CV asCv(JAXBElement<?> element) {
-		return (CV) element.getValue();
-	}
-
-	private Unmarshaller createUnmarshaller() throws JAXBException {
-		Class<?>[] types = { ObjectFactory.class };
-		JAXBContext c = new ContextCache().getContext(types);
-		return c.createUnmarshaller();
+		return super.unmarshall(xml);
 	}
 }
