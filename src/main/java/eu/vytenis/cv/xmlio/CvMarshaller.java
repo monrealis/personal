469d9c4d9172fe5aeaad64d45d335e131c2530cb
Vytenis Bivainis <monrealis@gmail.com>
2016-02-01 00:44:54 +0200

    Make ObjectFactory a field.

--- src/main/java/eu/vytenis/cv/xmlio/CvMarshaller.java
+++ src/main/java/eu/vytenis/cv/xmlio/CvMarshaller.java
@@ -4,11 +4,13 @@ import eu.vytenis.cv.CV;
 import eu.vytenis.cv.ObjectFactory;
 
 public class CvMarshaller extends BaseMarshaller {
+	private final ObjectFactory factory = new ObjectFactory();
+
 	public CvMarshaller() {
 		super(ObjectFactory.class);
 	}
 
 	public String marshall(CV cv) {
-		return marshallObject(new ObjectFactory().createCv(cv));
+		return marshallObject(factory.createCv(cv));
 	}
 }
